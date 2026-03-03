fe5bcd60eab55c5654f90ecfd0252bda4273f9bb
Shariq Mirza <shariqmirza433@gmail.com>
2026-03-03 11:05:41 +0530

    fix jwt extractor

--- backend/src/auth/jwt.strategy.ts
+++ backend/src/auth/jwt.strategy.ts
@@ -1,17 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
-import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Strategy } from 'passport-jwt';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
-      jwtFromRequest: ExtractJwt.fromExtractors([
-        (req) => {
-          return req?.cookies?.token;
-        },
-      ]),
-      ignoreExpiration: false,
+      jwtFromRequest: (req) => {
+        if (!req || !req.cookies) return null;
+        return req.cookies['token'];
+      },
       secretOrKey: process.env.JWT_SECRET,
     });
   }
