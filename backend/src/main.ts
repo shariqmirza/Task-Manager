7facbafb358b9d6baeb886d99e5f9a7d6818676e
Shariq Mirza <shariqmirza433@gmail.com>
2026-03-03 08:28:27 +0530

    fix production cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,11 @@ import cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: "http://localhost:3000",
+    origin: [ 
+      "http://localhost:3000",
+      "https://task-manager-six-ivory.vercel.app",
+
+    ],
   credentials: true,
   });
   app.use(cookieParser());
